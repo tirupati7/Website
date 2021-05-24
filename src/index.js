34c4e03d031b87aee09c1e04807e02f8cd54bb03
Vinodh Reddy Onti <vinuoye@Vinodhs-MacBook-Pro.local>
2021-05-24 12:49:29 +0530

    code cleanup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { BrowserRouter as Router, browserHistory } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Routes } from './routes/routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Provider } from 'react-redux';
