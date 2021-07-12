94bb74b0b218e0c0fc19a517d09d74dacc3e2bfa
Vinodh Reddy Onti <vinuoye@Vinodhs-MacBook-Pro.local>
2021-07-12 12:30:26 +0530

    updated detailed post view

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,11 +5,14 @@ import ContactUS from '../components/contactUS';
 import TouristPlaces from '../components/touristsPlaces';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Header from '../header/header';
+import Footer from '../components/footer';
 import DetailedPostView from '../components/detailedPostView';
+import ScrollToTop from "react-scroll-to-top";
 
 export const Routes = () => {   
     return (
         <div className="whole">
+            <ScrollToTop smooth color="#00cc66" />
             <Header />
             <Switch>
                 <Route exact path="/home" component={Home} />
@@ -19,8 +22,9 @@ export const Routes = () => {
                 <Route exact path="/about" component={About} />
                 <Route exact path="/touristPlaces" component={TouristPlaces} />
                 <Route exact path="/contact-us" component={ContactUS} />
-                <Route exact path="/detailed-post-view" component={DetailedPostView} />
+                <Route exact path="/detailed-post-view/:id" component={DetailedPostView} />
             </Switch>
+            <Footer/>
         </div>
     );
 };
